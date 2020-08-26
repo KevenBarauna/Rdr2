bddaad407b1f873b3e04fe01b6c9caf4b62a102b
Keven Baraúna <kpbarauna>
2020-08-26 10:07:36 -0300

    Destaque da Home - código limpo

--- src/Components/Destaque/Destaque.js
+++ src/Components/Destaque/Destaque.js
@@ -11,29 +11,34 @@ const Destaque = (props) => {
         padrao, //TRUE = IMAGEM ESQUERDA | FALSE = IMAGEM DIREITA [BOOL]
     } = props;
 
+    const renderEsquerda = () => (
+     <>
+        <Col sm={12} md={6} lg={6}>
+            <p className='destaque-titulo' style={{ marginLeft: '50%' }}>{titulo}</p>
+            <p className='destaque-sub-titulo' style={{ marginLeft: '50%' }}>{subTitulo}</p>
+        </Col >
+        <Col sm={12} md={6} lg={6}>
+            <img className='destaque-imagem' src={imagem} alt={altImagem}></img>
+        </Col>
+     </>
+    )
+
+    const renderDireita = () => (
+        <>
+            <Col sm={12} md={6} lg={6}>
+                <img className='destaque-imagem' style={{ marginLeft: '41%' }} src={imagem} alt={altImagem}></img>
+            </Col >
+            <Col sm={12} md={6} lg={6}>
+                <p className='destaque-titulo' style={{ marginRight: '50%' }}>{titulo}</p>
+                <p className='destaque-sub-titulo' style={{ marginRight: '50%' }}>{subTitulo}</p>
+            </Col>
+        </>
+    )
+
 
     return (
         <Row className='destaque-background'>
-            <Col sm={12} md={6} lg={6}>
-                {padrao === true ? (
-                    <>
-                        <p className='destaque-titulo' style={{ marginLeft: '50%' }}>{titulo}</p>
-                        <p className='destaque-sub-titulo' style={{ marginLeft: '50%' }}>{subTitulo}</p>
-                    </>
-                ) : (
-                        <img className='destaque-imagem' style={{ marginLeft: '41%' }} src={imagem} alt={altImagem}></img>
-                    )}
-            </Col >
-            <Col sm={12} md={6} lg={6}>
-                {padrao === true ? (
-                    <img className='destaque-imagem' src={imagem} alt={altImagem}></img>
-                ) : (
-                        <>
-                            <p className='destaque-titulo' style={{ marginRight: '50%' }}>{titulo}</p>
-                            <p className='destaque-sub-titulo' style={{ marginRight: '50%' }}>{subTitulo}</p>
-                        </>
-                    )}
-            </Col>
+            {padrao === true ? (renderEsquerda()) : (renderDireita())}
         </Row>
     );
 }
